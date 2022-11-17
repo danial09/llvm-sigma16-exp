ff9d103355ff7cc6b022ff4a26501021056815ae
Danial Tariq <danial09.dt@gmail.com>
2022-11-17 11:46:18 +0000

    Add basic Sigma16 instructions

---
+++ llvm/lib/Target/Sigma16/Sigma16.h
@@ -0,0 +1,26 @@
+//=-- Sigma16.h - Top-level interface for Sigma16 representation -*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the entry points for global functions defined in
+// the LLVM Sigma16 back-end.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_SIGMA16_SIGMA16_H
+#define LLVM_LIB_TARGET_SIGMA16_SIGMA16_H
+
+#include "MCTargetDesc/Sigma16MCTargetDesc.h"
+#include "llvm/Target/TargetMachine.h"
+
+namespace llvm {
+    class Sigma16TargetMachine;
+    class FunctionPass;
+} // namespace llvm
+
+#endif // LLVM_LIB_TARGET_SIGMA16_SIGMA16_H
